79f7ce49fe73dee31896286b3a56ca81faf75345
Justin Heideman <justin@fiddlyio.com>
2015-01-06 14:49:39 -0600

    fix issue with clips not loading on initial pageload

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,12 @@
 import Ember from "ember";
-import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
+//import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend({
     /**
      * Do initial data load from json file into store, which should be backed by localstorage
      */
     beforeModel: function(){
+        console.log('running before model');
         var store = this.store;
         return Ember.$.getJSON( this.get('router.rootURL') + 'audio.json').then(function(data) {
             data.forEach(function(item){
@@ -16,6 +17,3 @@ export default Ember.Route.extend({
         });
     }
 });
-
-
-export default Ember.Route.extend(ApplicationRouteMixin);
