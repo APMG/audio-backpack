c660091b45caed8485bbf3d625e3a893fbf07cd5
Justin Heideman <justin@fiddlyio.com>
2015-01-07 11:30:32 -0600

    put prettyDuration in correct spot

--- app/models/audio.js
+++ app/models/audio.js
@@ -7,4 +7,22 @@ import DS from "ember-data";
     duration: DS.attr(),
     apm_audio: DS.attr(),
     pub_date: DS.attr('date'),
+    
+    /**
+     * Gives a pretty duration for use in templates
+     */
+    prettyDuration: function(){
+        var dur = this.get('duration');
+        var hms = dur.split(':');
+        var ret;
+        if (hms[0] !== '00') {
+            ret = hms[0] + 'hour '+ hms[1] + 'min ' + hms[2] + 'sec';
+        } else if (hms[1] !== '00') {
+            ret =  hms[1] + 'min ' + hms[2] + 'sec';
+        } else {
+            ret = hms[2] + 'sec';
+        }
+        ret = ret.replace(' 0sec', '');
+        return ret;
+    }.property('prettyDuration'),
 });
