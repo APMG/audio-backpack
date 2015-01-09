934e6e7e99b2de950621982008f35659c57b85e0
Justin Heideman <justin@fiddlyio.com>
2015-01-09 14:07:31 -0600

    add last name computed property

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,12 @@
 import DS from "ember-data";
 
 export default DS.Model.extend({
-	first_name: DS.attr(),
-	last_name:  DS.attr(),
-	gravatar: DS.attr(),
-	playlists: DS.hasMany('playlist')
+    first_name: DS.attr(),
+    last_name:  DS.attr(),
+    gravatar: DS.attr(),
+    playlists: DS.hasMany('playlist'),
+
+    full_name: function() {
+        return this.get('first_name') + ' ' + this.get('last_name');
+    }.property('first_name', 'first_name')
 });
