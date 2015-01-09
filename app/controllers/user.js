0ffdba5364268815ddbc5855a3eced4835ffad2f
Justin Heideman <justin@fiddlyio.com>
2015-01-09 15:28:35 -0600

    work on user page

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -5,4 +5,12 @@ export default Ember.ObjectController.extend({
     playlists: function(){
         return this.model.store.find('playlist');
     }.property('playlists'),
+
+    /*
+     * cleans out annloying spaces that we do not want there 
+     */
+    ownerApos: function(){
+        var fname = this.get('model.first_name');
+        return fname.trim()+'\'s';
+    }.property('model.first_name')
 });
