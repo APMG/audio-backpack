039d99aa2bfbcc8abcebbd973cd545051a8b289c
Justin Heideman <justin@fiddlyio.com>
2015-01-09 15:05:53 -0600

    gravatar components

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -2,9 +2,7 @@ import Ember from "ember";
 
 export default Ember.ObjectController.extend({
     
-
     playlists: function(){
-        console.log('whut?');
         return this.model.store.find('playlist');
     }.property('playlists'),
 });
