5bec688d0f4016c4aaf26cb39da8e0fa481a0ec0
Justin Heideman <justin@fiddlyio.com>
2015-01-14 14:34:48 -0600

    more cleanup

--- app/router.js
+++ app/router.js
@@ -10,18 +10,14 @@ Router.reopen({
 });
 
 Router.map(function() {
-    this.resource('clips');
-    this.resource('clip', { path: '/clips/:clip_id' });
-    
+    this.resource('clips', function() {
+        this.route('clip', { path: '/clips/:clip_id' });
+    });    
     this.resource('lists', { path: '/playlists' }, function() {
         this.route('list', { path: '/playlists/:list_id' });
     });
-
-
     this.resource('login');
-
     this.resource('user', {path: '/user/:user_id'});
-
 });
 
 export default Router;
