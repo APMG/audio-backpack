1cdf594cede188fd4c43b8dd17bed2e30cb0807a
Justin Heideman <justin@fiddlyio.com>
2015-01-15 12:45:16 -0600

    keep adapters and serializers set up for testing apis as they come online

---
+++ app/serializers/list.js
@@ -0,0 +1,7 @@
+/**
+ * This may need to be customized to not cover the entire application, but rather the various models. 
+ */
+
+import DS from "ember-data";
+
+export default DS.RESTSerializer.extend();
