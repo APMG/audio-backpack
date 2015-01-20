3a8225e3fe476b5680fd5a83894939b599b2e90f
Justin Heideman <justin@fiddlyio.com>
2015-01-20 10:23:40 -0600

    clean up item payload

--- app/serializers/item.js
+++ app/serializers/item.js
@@ -3,21 +3,23 @@
  */
 
 import DS from "ember-data";
+import Ember from 'ember';
 
 export default DS.RESTSerializer.extend({
 
     normalizePayload: function(payload) {
-        console.log('item-payload',payload);
+        if (!Ember.isEmpty(payload.items)){
+            for (var i = 0; i < payload.items.length; i++) {
+                delete payload.items[i].created_at;
+                delete payload.items[i].updated_at;
+                delete payload.items[i].list_item_type_id;
+            }
+        }
         delete payload.list_item_types;
         delete payload.clip;
-        // delete payload.status;
+        delete payload.created_at;
+        delete payload.updated_at;
+        delete payload.list_item_type_id;
         return payload;
     },
-
-
-
 });
-
-/**
- * WARNING: The payload for 'item' contains these unknown keys: [created_at,updated_at,list_item_type_id]. Make sure they've been defined in your model.
- */
