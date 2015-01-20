700c03b33e085ddff159002d48e9ae8ff201d22b
Justin Heideman <justin@fiddlyio.com>
2015-01-20 16:22:40 -0600

    unused controller

--- app/controllers/items.js
+++
@@ -1,7 +0,0 @@
-import Ember from "ember";
-
-export default Ember.ArrayController.extend({
-    //model: ['items'],
-    sortProperties: ['position'],
-    sortAscending: false // false for descending
-});
