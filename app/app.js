2601ed04fb153f51750ee758f0f41fabfad9ebde
Justin Heideman <justin@fiddlyio.com>
2015-01-23 09:52:35 -0600

    better initial loading screen

--- app/app.js
+++ app/app.js
@@ -15,9 +15,8 @@ var App = Ember.Application.extend({
   LOG_TRANSITIONS: true,
   Resolver: Resolver,
   ready: function(){
-
         //take out the loading note
-        Ember.$("#loading-note, svg").remove();
+        Ember.$(".loading-note, svg").remove();
         var soundManager = new SoundManager();
         window.soundManager = soundManager;
         var settings = {
