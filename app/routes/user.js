8066ac8525c053f12378b723e0e62df42efbed5e
Justin Heideman <justin@fiddlyio.com>
2015-02-10 16:41:56 -0600

    make user playlist listing page paginate

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,11 +1,18 @@
 import Ember from 'ember';
+import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 import ENV from '../config/environment';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(RouteMixin, {
     model: function(params){
+        if (params.user_id) {
+            params.user = params.user_id;
+            delete params.user_id;
+        }
+        console.log(this.findPaged('list',params));
         return Ember.RSVP.hash({
-            user: this.store.find('user', params.user_id),
-            lists: this.store.find('list', {user: params.user_id})
+            user: this.store.find('user', params.user),
+            //lists: this.store.find('list', {user: params.user_id})
+            lists: this.findPaged('list',params)
         });
     },
     afterModel: function(model){
