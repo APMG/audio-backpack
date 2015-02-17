da0c7b06c87c6f6eb49f8913e885f44104b137c0
Justin Heideman <justin@fiddlyio.com>
2015-02-17 16:11:56 -0600

    inital work replacing FA

--- Brocfile.js
+++ Brocfile.js
@@ -45,10 +45,10 @@ app.import('bower_components/APMPlayer-Internal/script/src/apmplayer_init.js');
 //app.import('bower_components/bootstrap/less/bootstrap.less');
 
 //app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', { destDir: 'assets' });
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', { destDir: 'fonts' });
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', { destDir: 'fonts' });
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', { destDir: 'fonts' });
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', { destDir: 'fonts' });
+// app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', { destDir: 'fonts' });
+// app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', { destDir: 'fonts' });
+// app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', { destDir: 'fonts' });
+// app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', { destDir: 'fonts' });
 
 app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
